0967e81229695e320cd3fb016e7415ec57756fd6
Mark Michaelis <mark@IntelliTect.com>
2021-11-19 02:50:23 -0800

    Source code at start of talk

---
+++ 2. CallerArgumentExpression.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace ModernCSharpGuidelines
+{
+    public class Guard
+    {
+        static public string ThrowIfNullEmptyOrWhitespace(
+            string value, [CallerArgumentExpression("value")] string? argumentExpression = null!)
+        {
+            return string.IsNullOrWhiteSpace(value) ? 
+                throw new ArgumentException(
+                    $"'{argumentExpression}' cannot evaluate to null, empty, or whitespace.", 
+                    argumentExpression) : value;
+        }
+
+        static public string ThrowIfNull(
+            string? value,
+            [CallerArgumentExpression("value")] string? argumentExpression = null!) =>
+                value ?? throw new ArgumentNullException(argumentExpression);
+    }
+
+}
---
+++ 6. Summary.cs
@@ -0,0 +1,20 @@
+﻿
+/// <summary>
+/// 
+/// CONTACT INFO
+///     Mark Michaelis
+///     mark@IntelliTect.com
+///     fb.com/MarkMichaelis
+///     @MarkMichaelis
+/// 
+/// SOURCE CODE
+///     https://Github.com/IntelliTect-Samples/CSharpNullability
+/// 
+/// Guidelines for nullability with .NET 6.0/C# 10.0
+/// 
+/// OTHER SESSIONS
+///     An In-Depth Look at Programming With Nullability
+///
+/// </summary>
+
+
